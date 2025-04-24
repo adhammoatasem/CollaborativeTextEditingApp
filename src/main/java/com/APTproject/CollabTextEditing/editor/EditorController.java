395bfa27a9f3d0f3d7f018fdda41e2c457f4f953
adhammokka mahmoud <adham21206@hotmail.com>
2025-04-24 10:53:13 +0200

    WebSocket

--- src/main/java/com/APTproject/CollabTextEditing/editor/EditorController.java
+++ src/main/java/com/APTproject/CollabTextEditing/editor/EditorController.java
@@ -1,5 +1,6 @@
 package com.APTproject.CollabTextEditing.editor;
 
+import com.APTproject.CollabTextEditing.model.EditMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -8,5 +9,13 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class EditorController {
+
+    @MessageMapping("/edit")
+    @SendTo("/topic/EditMessage")
+    public EditMessage editMessage(@Payload EditMessage edit){
+        return edit;
+    }
+
+
 
 }
